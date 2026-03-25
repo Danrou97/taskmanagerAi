08215170532bd8ecec96523e3ae272c5ada6f417
Daniel <dani@example.com>
2026-03-24 22:36:01 -0500

    Pesistence with .json

--- main.py
+++ main.py
@@ -16,34 +16,36 @@ def print_menu():
 def main():
 
     manager =  TaskManager()
-    while True:
 
-        
-        print_menu()
-        choise = input("Take one option: ")
+    try:
+        while True:      
+            print_menu()
+            choise = int (input("Take one option: "))
 
-        match choise:
-            case "1":
-                description = input("Discribe your task: ")
-                manager.add_task(description)
-            case "2":
-                manager.list_tasks()
-                
-            case "3":
-                id = input("write the target id: ")
-                manager.complete_task(id)
-                
-                
-            case "4":
-                id = input("write the target id: ")
-                manager.delete_task(id)
-                
-            case "5":
-                print("Goodbye!")
-                break
-            case _:
-                print("Invalid option")
-            
+            match choise:
+                case 1:
+                    description = input("Discribe your task: ")
+                    manager.add_task(description)
+                case 2:
+                    manager.list_tasks()
+                    
+                case 3:
+                    id = int(input("write the target id: "))
+                    manager.complete_task(id)
+                    
+                    
+                case 4:
+                    id = int(input("write the target id: "))
+                    manager.delete_task(id)
+                    
+                case 5:
+                    print("Goodbye!")
+                    break
+                case _:
+                    print("Invalid option")
+    except ValueError as e:
+        print(f"Error: {e}, choose other option")
+               
 
 if __name__ == "__main__":
     main()
--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,6 @@
 #en este archivo se encuentra la clase TaskManager, que se encarga de gestionar las tareas del sistema, incluyendo la creación, eliminación y actualización de tareas.
+import json
+
 
 class Task:
     def __init__(self, id, description, completed = False):
@@ -9,20 +11,24 @@ class Task:
     # Redefinimos la funcion __str__
 
     def __str__(self):
-        status = "✔" if self.completed else " "
+        status = "✔ " if self.completed else " "
         return f"[{status}] #{self.id}: {self.description}"
     
 
 class TaskManager :
-
+    FILENAME = "task.json" # En el futuro la persistencia debe venier de una DB SQL o NoneSQL
     def __init__(self):
         self._tasks = [] #en el futuro deberiamos usar dics 
         self._next_id = 1
-    
+        self.load_tasks()
+
+
+
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id +=1
+        self.save_tasks()
         print(f"Task has been added: {description} with id: {task.id}") # en el futuro deberian ser logs 
 
     def list_tasks(self):
@@ -37,14 +43,38 @@ class TaskManager :
         for tsk in self._tasks:
             if tsk.id == id:
                 tsk.completed = True 
+                self.save_tasks()
                 print(f"Task finished: {tsk}")
                 return
         print (f"Task don't find: #{id}")
+
+
     def delete_task(self, id):
         for tsk in self._tasks:
             if tsk.id == id:
                 description = tsk.description
                 self._tasks.remove(tsk) 
+                self.save_tasks()
                 print(f"Task has been deleted: #{id}: {description}")
                 return
         print (f"Task don't find:#{id}")
+
+    # crearemos la persistencia de el programa
+
+    def load_tasks(self):
+        try:
+            with open(self.FILENAME, "r") as file:
+                data = json.load(file)
+                self._tasks = [Task(item["id"], item["description"], item["completed"]) for item in data]
+                if self._tasks:
+                    self._next_id = self._tasks[-1].id + 1
+        except FileNotFoundError:
+            self._tasks = []
+
+    def save_tasks(self):
+        try:
+            with open(self.FILENAME, "w") as file:
+                json.dump([{"id": task.id , "description" : task.description, "completed" : task.completed  } for task in self._tasks], file, indent= 4 )
+                
+        except FileNotFoundError as e :
+            print("Error as : {e} ")
